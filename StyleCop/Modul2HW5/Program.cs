ce4018e9d7066301ecf7c0ca892085dc2a56427f
Dimasik2001 <dimonkalibaba@gmail.com>
2021-07-18 13:15:39 +0300

    Empty Project

---
+++ StyleCop/Modul2HW5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Modul2HW5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
